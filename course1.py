12f17913b599cf664a5ecbec62fc255e73643378
mahmoudhafez103 <97672919+mahmoudhafez103@users.noreply.github.com>
2022-08-07 20:57:51 +0200

    course sessions

---
+++ course1.py
@@ -0,0 +1,18 @@
+# interpreted lang errors
+"""
+any device run with py has micro os to manege hardware resourses
+key words "py ->22
+c++ -> 122
+
+indentation = tabs 
+varables = pointer on momery
+data type 
+**primative
+(boolean-int-float-char)
+**non-primative
+(list-string-)
+every thing in py is object(has valand behavior)
+"""
+x = 10
+y = 6
+z = x+y
---
+++ course2.py
@@ -0,0 +1,40 @@
+"""    
+print(r"result is: {x-y}")
+print("result is:" + str(x*y))
+
+"""
+
+# x = int(input("enter the first number: "))
+# y = int(input("enter the second number: "))
+# z = input("enter the operation:")
+# if z == '+':
+#     print(r"result is:{x+y}")
+# elif z == '-':
+#     print(r"result is: {x-y}")
+# elif z == '*':
+#     print(r"result is:{x*y}")
+# elif z == '/':
+#     print(r"result is: {x/y}")
+# else:
+#     print("invaild operation......!")
+
+
+# i = 1
+# for (i=0, i):
+#     print(i, end=" ")
+#     i += 1
+
+
+# t = int(input("Enter the number of inputs : "))
+# sum = 0
+# i = 1
+# while i <= t:
+#     sum += int(input(f'Enter the {i} number : '))
+#     i += 1
+# print(f"the sum is : {sum}")
+
+
+_list = ["mahmoud", "molham", "omar", "Ahmed"]
+# for i in range(0, len(_list)):
+_list.pop()
+print(_list[:])
